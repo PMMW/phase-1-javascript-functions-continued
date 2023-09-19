670eb1068a37791c38433de90fe3694d6851af2d
Morrison Wanja <morrisonwanja@gmail.com>
2023-09-19 21:35:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function mondayWork(defaultStatement="go to the office"){
+    return `This Monday, I will ${defaultStatement}.`;
+}
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+
+}
+
+
+// function wrapAdjective(wrapper){
+//     let wrapper1 = "*";
+//     let wrapper2 ="||";
+// if(wrapper1)
+//  return `Your are ${wrapper1}a hard worker${wrapper1}!`;  
+
+// }
+
+// function wrapAdjective(wrapper){
+
+// }
+function wrapAdjective(wrapper) {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
